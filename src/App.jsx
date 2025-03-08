f8e43fa41c3623cc3fb39d4a078a8c4ee4a442b5
Arun1only1 <arun1nly1@gmail.com>
2025-03-08 10:55:22 +0545

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 import React from "react";
-import Navbar from "./Components/Navbar/Navbar";
-import Hero from "./Components/Hero/Hero";
-import Product from "./Components/Products/Products";
-import Title from "./Components/Title/Title";
-import About from "./Components/About/About";
-import Contact from "./Components/Contact/Contact";
-import Footer from "./Components/Footer/Footer";
+import Dashboard from "./pages/Dashboard";
+import { Route, Routes } from "react-router";
+import Product from "./pages/Product";
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <div className="container">
-        <Title subTitle="Our Product" title="What We Offer" />
-        <Product />
-        <About />
-
-        <Title subTitle="Contact us" title="Get In Touch" />
-        <Contact />
-      </div>
-      <Footer />
-    </div>
+    <Routes>
+      <Route index element={<Dashboard />} />
+      <Route path="/product" element={<Product />} />
+    </Routes>
   );
 };
 
